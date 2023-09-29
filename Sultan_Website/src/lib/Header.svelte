c2b3f3d5f0382bd704a609709f11c28ebd54c2a2
Sultan Priesa <sultanpriesa@gmail.com>
2023-09-29 14:53:11 +1300

    move nav to header

--- Sultan_Website/src/lib/Header.svelte
+++ Sultan_Website/src/lib/Header.svelte
@@ -1,3 +1,19 @@
 <div>
     This is a header
 </div>
+<nav>
+    <ul> 
+        <li>
+            <a href="/">Home</a>
+        </li>
+        <li>
+            <a href="/About">About</a>
+        </li>
+        <li>
+            <a href="/Team">Team</a>
+        </li>
+        <li>
+            <a href="/Contact">Conact</a>
+        </li>
+    </ul>
+</nav>
--- Sultan_Website/src/routes/+layout.svelte
+++ Sultan_Website/src/routes/+layout.svelte
@@ -7,22 +7,7 @@
     import Footer from "$lib/Footer.svelte"
 </script>
    
-<nav>
-    <ul> 
-        <li>
-            <a href="/">Home</a>
-        </li>
-        <li>
-            <a href="/About">About</a>
-        </li>
-        <li>
-            <a href="/Team">Team</a>
-        </li>
-        <li>
-            <a href="/Contact">Conact</a>
-        </li>
-    </ul>
-</nav>
+
 
 <Header />
 <slot />
