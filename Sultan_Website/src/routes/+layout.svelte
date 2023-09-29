d8505f33812a9789da95da4d875a101fd183891e
Sultan Priesa <sultanpriesa@gmail.com>
2023-09-29 14:43:49 +1300

    fixing the nav bar

--- Sultan_Website/src/routes/+layout.svelte
+++ Sultan_Website/src/routes/+layout.svelte
@@ -2,14 +2,27 @@
     <link rel="stylesheet" href="css\main.css"/>
 </svelte:head>
 
-
-
 <script>
     import Header from "$lib/Header.svelte"
     import Footer from "$lib/Footer.svelte"
 </script>
    
-
+<nav>
+    <ul> 
+        <li>
+            <a href="/">Home</a>
+        </li>
+        <li>
+            <a href="/About">About</a>
+        </li>
+        <li>
+            <a href="/Team">Team</a>
+        </li>
+        <li>
+            <a href="/Contact">Conact</a>
+        </li>
+    </ul>
+</nav>
 
 <Header />
 <slot />
--- Sultan_Website/src/routes/+page.svelte
+++ Sultan_Website/src/routes/+page.svelte
@@ -1,21 +1,2 @@
-<!-- <div>
-    this is the middle part
-</div> -->
-<nav>
-    <ul> 
-        <li>
-            <a href="/">Home</a>
-        </li>
-        <li>
-            <a href="/About">About</a>
-        </li>
-        <li>
-            <a href="/Team">Team</a>
-        </li>
-        <li>
-            <a href="/Contact">Conact</a>
-        </li>
-    </ul>
-</nav>
 
 Home
