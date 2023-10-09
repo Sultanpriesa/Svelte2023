5913b01c7a8b5c5a24ed6b0e8e3ed276bf3cd179
Sultan Putra Priesa (1000119406) <PRIES3@student.op.ac.nz>
2023-10-09 17:34:45 +1300

    renamed file and fixed the home page

--- Sultan_Website/src/routes/+page.svelte
+++ Sultan_Website/src/routes/+page.svelte
@@ -1,2 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
 
-Home
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="style.css">
+    <title>Sultan web assignment, Sultan Portfolio Page</title>
+</head>
+
+<body>
+    <header>
+        <div id="particles"></div>
+        <div class="info-text">
+            <h1>Sultan Priesa</h1>
+            <h3>Year 1 Programmer at Otago Polytechnic</h3>
+            <a href="about" class="scroll"><i class='bx bxs-down-arrow'></i></a>
+        </div>
+    </header>
+</body>
+
+</html>
