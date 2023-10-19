5e7fc93fb4662da8e8776807fbb9c818bd0bd5a0
Sultan Putra Priesa (1000119406) <PRIES3@student.op.ac.nz>
2023-10-19 15:11:15 +1300

    unfinished header and starting on footer

--- Sultan_Website/src/lib/Footer.svelte
+++ Sultan_Website/src/lib/Footer.svelte
@@ -1,10 +1,68 @@
 <script>
     import Nav from "./Nav.svelte"
 </script>
+<!-- <Nav /> -->
 
 <footer>
-    <div class="footerText"> 
-        <Nav />
+    <div class="copyright"> 
+        <span>
+            This page is written by Sultan  Priesa
+        </span>
+        <span>
+            © 2023 Sultan Priesa, All Rights reserved.
+        </span>
     </div>
-
+    <div class="footer-Button">
+        <div class="button-to-top">
+            <span>▲</span>
+        </div>
+    </div>
 </footer>
+
+<style>
+footer{
+    background-color: black;
+    position: fixed;
+    z-index: 96;
+    bottom: 0;
+    left: 0;
+    float: none;
+    width: 100%;
+    box-sizing: border-box;
+    justify-content: space-between;
+    box-shadow: 27px 20px 15px gray;
+}
+.copyright{    
+    font-size: 13px;
+    line-height: 13px;
+    float: left;
+    width: auto;
+    text-align: left;
+    /* display: -webkit-flex; */
+    flex-wrap: nowrap;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+
+    color: rgb(218, 21, 21);
+}
+.copyright span{
+    float: left;
+    width: 100%;
+    padding: 2px;
+}
+
+.button-to-top{
+    min-width: 30px;
+    height: 30px;
+    margin-left: 10px;
+    padding: 7px;
+    
+    color: white;
+    
+
+}
+
+
+
+</style>
--- Sultan_Website/src/lib/Header.svelte
+++ Sultan_Website/src/lib/Header.svelte
@@ -1,112 +1,105 @@
 <header class="header-wrap">
-    <div class="logo">
+    <div class="container">
         <!-- svelte-ignore a11y-img-redundant-alt -->
         <img class="header-image" 
         src="https://seeklogo.com/images/F/fia-formula-one-world-championship-logo-315707FDD7-seeklogo.com.png"
         alt="Formula 1 image">
     </div>
-    <nav>
-        <ul class="menu"> 
-            <li>
-                <a href="/">Home</a>
-            </li>
-            <li>
-                <a href="/About">About</a>
-            </li>
-            <li>
-                <a href="/features">Features</a>
-            </li>
-            <li>
-                <a href="/Contact">Conact</a>
-            </li>
-        </ul>
-    </nav>
+    <div class="menu">
+        <nav>
+            <ul class="menu-contents"> 
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/About">About</a>
+                </li>
+                <li>
+                    <a href="/features">Features</a>
+                </li>
+                <li>
+                    <a href="/Contact">Conact</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
 </header>
 
 <style>
-    .logo{
-        size: 20px;
-    }
-    Nav body{
-        margin: 0;
-        height: 100vh;
-        display: flex;
-        justify-content: flex;
-        align-items: center;
-        font-family: sans-serif;
-        background-color: #08090B;
-    }
-    
-    Nav ul
-    {
-        margin: 0;
-        padding: 0;
-        display: flex;
-        justify-content: flex-end;
-        list-style-type: none;
-    }
-    
-    
-    Nav ul li {
-        margin-right: 50px; 
-    }
-    Nav ul li a
-    {
-        color: rgb(218, 208, 208);
-        font-size: 35px;
-        text-decoration: none;
-        text-transform: uppercase;
-        padding: 5px 10px;
-        margin: 0px 10 px;
-        position: relative;
-        transition: all .5s;
-    }
+.header-wrapper {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    background-color: #333;
+    padding: 10px;
+}
+.container{
+    max-width: 320px;
+    height: auto;
+    display: block;
+}
+.header-image{
+    padding:10px;
+}
+.menu{
+    max-width: 1200px;
+    justify-content: flex-end;
+}
+.menu-contents{
+    display: inline-flex;
+    text-align: right;
+}
+Nav ul li {
+    margin: 5px; 
+}
+Nav ul li a{
+    color: red;
+    font-size: 35px;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 5px 10px;
+    margin: 0px 10 px;
+    position: relative;
+    transition: all .5s;
+}
+Nav a:hover{
+    background-color: rgb(231, 19, 19);
+    color: rgb(17, 16, 16);
+}
 
-    Nav a:hover
+Nav a::before{
+    content: "";
+    position: absolute;
+    bottom: 12px;
+    left: 12px;
+    height: 12px;
+    width: 12px;
+    border: 3px solid rgb(20, 19, 19);
+    border-width: 0 0 3px 3px;
+    opacity: 0;
+    transition: all .3s;
+}
+Nav a::after{
+    content: "";
+    position: absolute;
+    bottom: 12px;
+    left: 12px;
+    height: 12px;
+    width: 12px;
+    border: 10px solid black;
+    border-width: 0 0 3px 3px;
+    opacity: 0;
+    transition: all .3s;
+} 
+Nav a:hover::before{
+    opacity: 1;
+    bottom: -8px;
+    left: -8px;
+}
+Nav a:hover::after
     {
-        background-color: rgb(231, 19, 19);
-        color: white;
-    }
-    
-    Nav a::before
-    {
-        content: "";
-        position: absolute;
-        bottom: 12px;
-        left: 12px;
-        height: 12px;
-        width: 12px;
-        border: 3px solid rgb(231, 19, 19);
-        border-width: 0 0 3px 3px;
-        opacity: 0;
-        transition: all .3s;
-    }
-    
-    Nav a::after
-    {
-        content: "";
-        position: absolute;
-        bottom: 12px;
-        left: 12px;
-        height: 12px;
-        width: 12px;
-        border: 3px solid rgb(231, 19, 19);
-        border-width: 0 0 3px 3px;
-        opacity: 0;
-        transition: all .3s;
-    }
-    
-    Nav a:hover::before
-    {
-        opacity: 1;
-        bottom: -8px;
-        left: -8px;
-    }
-    
-    Nav a:hover::after
-    {
-        opacity: 1;
-        bottom: -8px;
-        left: -8px;
-    }
+    opacity: 1;
+    bottom: -8px;
+    left: -8px;
+}
     </style>
