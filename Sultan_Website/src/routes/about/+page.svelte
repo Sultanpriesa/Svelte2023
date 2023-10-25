4353f8fa74b648ac21abc404aac7e74144f52464
Sultan Putra Priesa (1000119406) <PRIES3@student.op.ac.nz>
2023-10-26 10:32:37 +1300

    added more information up to 1998  to the array

--- Sultan_Website/src/routes/about/+page.svelte
+++ Sultan_Website/src/routes/about/+page.svelte
@@ -18,5 +18,18 @@
       	description: "Graham Hill secured his first World Championship title in 1962, becoming the only driver to win the 'Triple Crown' of motorsport.",
       	imageUrl: "http://en.espnf1.com/PICTURES/CMS/900/959.jpg",
 	},
+	{
+      year: 1976,
+      event: "Niki Lauda's Comeback",
+      description: "Niki Lauda's remarkable recovery from a near-fatal crash in 1976 and his rivalry with James Hunt became one of the most iconic moments in F1 history.",
+      imageUrl: "https://static.standard.co.uk/s3fs-public/thumbnails/image/2019/05/21/03/nikilauda2105a.jpg?width=1200",
+    },
+	{
+      year: 1988,
+      event: "McLaren's Dominance",
+      description: "McLaren's MP4/4 car, driven by Ayrton Senna and Alain Prost, won 15 out of 16 races in the 1988 season, showcasing their dominance.",
+      imageUrl: "https://garagedreams.net/wp-content/uploads/2020/12/McLaren-MP4-4-Canada-Pal-Lannuier.jpg",
+    },
+
 ];
 </script>
