4b6c7d4d6bd806b78be0660b7364fe0e240eb8ce
Sultan Putra Priesa (1000119406) <PRIES3@student.op.ac.nz>
2023-10-26 10:14:12 +1300

    started making array for f1 history that needds to be displayed in the about section

--- Sultan_Website/src/routes/about/+page.svelte
+++ Sultan_Website/src/routes/about/+page.svelte
@@ -1,5 +1,10 @@
-<!-- <script>
-	let name = 'sultan';
+<script>
+	let f1History = [ 
+	{
+		year: 1950,
+		event: "First F1 World Championship",
+		description: "The FIA Formula One World Championship is a high-speed, premier auto racing series. It began in 1950 with the inaugural season, featuring seven races.",
+		imageUrl: "https://images.squarespace-cdn.com/content/v1/52744b67e4b0782c048b666f/1554311649477-8IRDL5Q6OSB7SWEMAIN1/1950+British+GP+start.jpg?format=1500w",
+	},
+];
 </script>
-
-<h1>Hello {name.toUpperCase()}!</h1>  -->
