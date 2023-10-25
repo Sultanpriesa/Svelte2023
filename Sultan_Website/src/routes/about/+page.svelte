67e33d6bc673f55b04b896585520f749588d1a27
Sultan Putra Priesa (1000119406) <PRIES3@student.op.ac.nz>
2023-10-26 10:22:49 +1300

    added more information to the array

--- Sultan_Website/src/routes/about/+page.svelte
+++ Sultan_Website/src/routes/about/+page.svelte
@@ -6,5 +6,17 @@
 		description: "The FIA Formula One World Championship is a high-speed, premier auto racing series. It began in 1950 with the inaugural season, featuring seven races.",
 		imageUrl: "https://images.squarespace-cdn.com/content/v1/52744b67e4b0782c048b666f/1554311649477-8IRDL5Q6OSB7SWEMAIN1/1950+British+GP+start.jpg?format=1500w",
 	},
+	{
+		year: 1954,
+		event: "Mercedes Dominance",
+		description: "Mercedes dominated in the 1954 season, with Juan Manuel Fangio winning his second consecutive title.",
+		imageUrl: "https://i.kinja-img.com/image/upload/c_fill,h_675,pg_1,q_80,w_1200/0f2fdd385a29303b34f23e1de425ed55.jpg",
+	},
+	{
+		year: 1962,
+      	event: "Graham Hill's Triumph",
+      	description: "Graham Hill secured his first World Championship title in 1962, becoming the only driver to win the 'Triple Crown' of motorsport.",
+      	imageUrl: "http://en.espnf1.com/PICTURES/CMS/900/959.jpg",
+	},
 ];
 </script>
