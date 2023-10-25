ea98f69fdba4138b8cab751abdff4616bb12c284
Sultan Putra Priesa (1000119406) <PRIES3@student.op.ac.nz>
2023-10-26 10:48:14 +1300

    added more information up to 2020 to the history array

--- Sultan_Website/src/routes/about/+page.svelte
+++ Sultan_Website/src/routes/about/+page.svelte
@@ -30,6 +30,29 @@
       description: "McLaren's MP4/4 car, driven by Ayrton Senna and Alain Prost, won 15 out of 16 races in the 1988 season, showcasing their dominance.",
       imageUrl: "https://garagedreams.net/wp-content/uploads/2020/12/McLaren-MP4-4-Canada-Pal-Lannuier.jpg",
     },
-
+	{
+      year: 1994,
+      event: "Ayrton Senna's Tragic Death",
+      description: "The tragic death of Ayrton Senna at the San Marino Grand Prix in 1994 shocked the world and led to significant safety improvements in F1.",
+      imageUrl: "https://s.france24.com/media/display/552ca688-0960-11e9-bb9c-005056a964fe/w:1280/p:16x9/AYRTON-senna.gif",
+    },
+    {
+      year: 1997,
+      event: "Hakkinen's First Title",
+      description: "Mika Hakkinen secured his first World Championship title with McLaren in 1997, marking the beginning of his successful F1 career.",
+      imageUrl: "https://www.f1i.com/wp-content/uploads/2017/05/mika-hakkinen-jerez-1997-e1493629748794.jpg",
+    },
+	{
+      year: 2010,
+      event: "Sebastian Vettel's Youngest World Champion",
+      description: "Sebastian Vettel became the youngest Formula 1 World Champion in 2010 while driving for Red Bull Racing, securing the title at the age of 23.",
+      imageUrl: "https://www.emirates247.com/polopoly_fs/1.317566.1452316302!/image/image.gif",
+    },
+	{
+      year: 2020,
+      event: "Lewis Hamilton's Record-Equaling Seventh Title",
+      description: "In 2020, Lewis Hamilton equaled Michael Schumacher's record with his seventh World Championship title, cementing his status as one of the greatest F1 drivers in history.",
+      imageUrl: "https://i.eurosport.com/2020/11/15/2937217-60300308-2560-1440.jpg",
+    },
 ];
 </script>
